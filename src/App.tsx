722c02c5e79c16aa1863682a00a21ae9484e7fe4
paulichdom <paulic.domagoj@gmail.com>
2025-03-16 13:54:17 +0100

    integrate react-query for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import '@mantine/core/styles.css';
-
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
+import '@mantine/core/styles.css';
+
+const queryClinet = new QueryClient();
+
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <QueryClientProvider client={queryClinet}>
+        <Router />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import { ArticlesCardsGrid } from '../ArticlesCardsGrid/ArticlesCardsGrid';
 import { MainLayout } from '../MainLayout/MainLayout';
 
 export function Home() {
+  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+  const apiVersion = 'api/v1';
+  const resourcePath = 'articles';
+  const url = `${apiBaseUrl}/${apiVersion}/${resourcePath}`;
+
+  const query = useQuery({
+    queryKey: ['articles'],
+    queryFn: async () => {
+      const response = await fetch(url);
+      return response;
+    },
+  });
+
   return (
     <MainLayout>
       <ArticlesCardsGrid />
