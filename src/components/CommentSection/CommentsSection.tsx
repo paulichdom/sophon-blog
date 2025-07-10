cebdd1af14aa291d46e7e881fee5e4f69a55614d
paulichdom <paulic.domagoj@gmail.com>
2025-07-10 18:10:23 +0200

    integrate user data into comments section component

--- src/components/CommentSection/CommentsSection.tsx
+++ src/components/CommentSection/CommentsSection.tsx
@@ -1,20 +1,22 @@
 import { FC, Fragment } from 'react';
 import { Divider } from '@mantine/core';
+import { UserData } from '@/auth/auth.types';
 import { CommentData } from '@/types/types';
 import { Comment } from '../Comment/Comment';
 import { CommentEditor } from '../CommentEditor/CommentEditor';
 
 type CommentsSectionProps = {
   articleSlug: string;
+  user: UserData;
   comments: CommentData[];
 };
 
-export const CommentsSection: FC<CommentsSectionProps> = ({ articleSlug, comments }) => {
+export const CommentsSection: FC<CommentsSectionProps> = ({ articleSlug, comments, user }) => {
   const hasComments = comments.length > 0;
 
   return (
     <Fragment>
-      <CommentEditor articleSlug={articleSlug} />
+      <CommentEditor articleSlug={articleSlug} user={user} />
       {hasComments && <Divider mt="xl" mb="xl" />}
       {hasComments &&
         comments.map((comment, index, commentsList) => (
