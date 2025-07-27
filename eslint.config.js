c9fc80bf120095e96f303b2841091e844e382611
paulichdom <paulic.domagoj@gmail.com>
2025-07-27 18:45:16 +0200

    configure typescript and eslint to warn about unused locals and parameters

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,16 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(...mantine, {
   ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    // Warn about unused imports specifically
+    'no-unused-vars': 'off', // Turn off base rule as it conflicts with TypeScript rule
+  },
 });
